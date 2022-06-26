21cae516908a2933c3d0b035869baf2f92fabfe6
Lucas Queirolo dos Santos <lq.santos@hotmail.com>
2022-06-25 22:40:10 -0300

    codigo e coisas aaaaah

--- Scripts/Drop.gd
+++ Scripts/Drop.gd
@@ -2,10 +2,14 @@ extends Node2D
 
 export var type = ""
 export var index = 0
+onready var tween = $Tween
 
 
 func _ready():
-	pass # Replace with function body.
+	tween.interpolate_property(self,"position", position, position + Vector2(0,50), 1,Tween.TRANS_BOUNCE,Tween.EASE_OUT)
+	tween.start()
+	yield(get_tree().create_timer(1.2), "timeout")
+	$Area2D/CollisionShape2D.disabled = false
 
 
 func _on_Area2D_body_entered(body):
--- Scripts/Resources.gd
+++ Scripts/Resources.gd
@@ -39,7 +39,7 @@ func Destroy(resourceType):
 		drop_instance.index = 0
 	elif type == "Stone":
 		drop_instance.index = 1
-	drop_instance.position = position
+	drop_instance.position = position - Vector2(0,20)
 	get_owner().get_node("YSort/Player").currentResource.erase(self)
 	print(get_owner().get_node("YSort/Player").currentResource.erase(self))
 	get_parent().add_child(drop_instance)
